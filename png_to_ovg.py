ea999cef8c7f09d3c3a83f8825ec42e773c96b91
Paul Curry <github@cr3ation.co.uk>
2021-06-23 10:28:27 +0100

    flatten the logic down

--- png_to_ovg.py
+++ png_to_ovg.py
@@ -13,46 +13,63 @@ pixels = list(img.getdata())
 print(f"Total pixels: {len(pixels)}")
 
 lastPixel = None
-groupCount = 0
-currentUniques = 0
+repeatCount = 0
+uniqueCount = 0
 uniqueOutput = ""
 
 bytesOut = bytearray()
 
-for pixel in pixels:
-    if pixel == lastPixel:
-        # Pixel was the same. Start a group.
-        groupCount = groupCount + 1
+
+def construct_bitstream(count, pixels, compressed):
+    if compressed:
+        # rle compressed repetition
+        while count > 127:
+            # Make sure we don't overflow
+            count = count - 127
+            cmdBlock = "11111111"
+            print(f"Bitstream: Command block {cmdBlock} then 127 pixels, all {pixels}")
+        if count > 0:
+            # Print any remainder now that the 127 blocks are dealt with
+            cmdBlock = "1" + binary_repr(count, 7)
+            print(f"Bitstream: Command block {cmdBlock} then {count} pixels, all {pixels}")
+        return True
     else:
-        # A new pixel colour!
-        if groupCount == 0:
-            # First pixel of the image; record it
-            lastPixel = pixel
+        # uniques, possibly more than one!
+        cmdBlock = "0" + binary_repr(count, 7)
+        print(f"Bitstream: Command block {cmdBlock} then {count} unique pixels follow: {pixels}")
+        return True
+
+
+# Loop over every pixel in the loaded image
+for pixel in pixels:
+    # Pixel was the same. Add to the tally and continue.
+    if pixel == lastPixel:
+        repeatCount = repeatCount + 1
+        continue
+    # A new pixel colour!
+    if repeatCount == 0:
+        # First pixel of the image; record it
+        lastPixel = pixel
+        continue
+    if repeatCount > 1:
+        if uniqueCount > 0:
+            # We just ended a run of unique pixels.
+            print(f"> {uniqueCount}\t unique pixels ready - needs pixel count but no RLE repeat flag")
+            # Construct output
+            construct_bitstream(uniqueCount, uniqueOutput, 0)
+            # Reset unique accumulators
+            uniqueOutput = ""
+            uniqueCount = 0
             continue
-        if groupCount > 1:
-            if currentUniques > 0:
-                # We just ended a run of unique pixels. Terminate it, output it.
-                print(f"> {currentUniques}\t unique pixels ready - needs pixel count but no RLE repeat flag")
-                cmdBlock = "0" + binary_repr(currentUniques, 7)
-                print(f"Bitstream: Command block {cmdBlock} then {currentUniques} unique pixels follow:{uniqueOutput}")
-                uniqueOutput = ""
-                currentUniques = 0
-                continue
-            # We just ended a run of the same pixels. Terminate it, output it
-            print(f"> {groupCount}\t repeat pixels ready - needs pixel count and RLE repeat flag")
-            while groupCount > 127:
-                # Make sure we don't overflow
-                groupCount = groupCount - 127
-                cmdBlock = "11111111"
-                print(f"Bitstream: Command block {cmdBlock} then 127 pixels, all {pixel}")
-            if groupCount > 0:
-                # Print any remainder now that the 127 blocks are dealt with
-                cmdBlock = "1" + binary_repr(groupCount, 7)
-                print(f"Bitstream: Command block {cmdBlock} then {groupCount} pixels, all {pixel}")
-        else:
-            # This is a unique pixel; start a run of uniques
-            currentUniques = currentUniques + 1
-            uniqueOutput = f"{uniqueOutput} {pixel}"
-        groupCount = 0
+        # We just ended a run of repeated pixels
+        print(f"> {repeatCount}\t repeat pixels ready - needs pixel count and RLE repeat flag")
+        # Construct output
+        construct_bitstream(repeatCount, pixel, 1)
+        repeatCount = 0
+        continue
+    # This is a unique pixel; start a run of uniques
+    uniqueCount = uniqueCount + 1
+    uniqueOutput = f"{uniqueOutput} {pixel}"
+    repeatCount = 0
 
 # to do: construct bitstream; the above human-readable output appears correct
